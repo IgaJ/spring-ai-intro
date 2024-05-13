8fdb829e48eb132619c8fbed6cd18eee24b3fdb6
IgaJ <i.jarzewicz@gmail.com>
2024-05-13 13:40:05 +0200

    adding custom response format example

--- src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
@@ -27,6 +27,17 @@ public class OpenAIServiceImpl implements OpenAIService{
     @Value("classpath:templates/get-capital-prompt.st")
     private Resource getCapitalPrompt;
 
+    @Value("classpath:templates/get-capital-with-info.st")
+    private Resource getCapitalWithInfo;
+
+    @Override
+    public Answer getCapitalWithInfo(GetCapitalRequest getCapitalRequest) {
+        PromptTemplate promptTemplate = new PromptTemplate(getCapitalWithInfo);
+        Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()));
+        ChatResponse response = chatClient.call(prompt);
+
+        return new Answer(response.getResult().getOutput().getContent());
+    }
 
     @Override
     public Answer getCapital(GetCapitalRequest getCapitalRequest) {
